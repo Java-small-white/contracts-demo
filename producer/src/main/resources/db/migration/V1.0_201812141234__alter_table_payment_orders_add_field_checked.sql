4d7d6dcb3d49b5c0c839286bb97947e998fa4887
JiaNing Huang <healthyxiaobai@gmail.com>
2020-04-24 12:13:38 +0800

    [DEL]无用SQL

--- producer/src/main/resources/db/migration/V1.0_201812141234__alter_table_payment_orders_add_field_checked.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `payment_orders`
-  ADD COLUMN `is_checked` bit(1) NOT NULL COMMENT '是否已经对账' AFTER `bank_parameters`;
-  
-ALTER TABLE `payment_orders`
-  ADD COLUMN `check_result` varchar(250) DEFAULT NULL COMMENT '对账结果' AFTER `is_checked`;
-  
-ALTER TABLE `payment_orders` ADD INDEX idx_payment_orders_checked(order_time, is_checked);
-
